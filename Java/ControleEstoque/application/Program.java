609a8db6ebeff4b8fab8fe9a47352173c65c0655
Rafael Pastor <rafa.f.pastor@gmail.com>
2022-04-01 17:46:36 -0300

    Excluido arquivo Inventory, criadas variaveis, métodos, gets e sets

--- Java/ControleEstoque/application/Program.java
+++ Java/ControleEstoque/application/Program.java
@@ -2,7 +2,40 @@ package application;
 
 import java.util.Scanner;
 
+import entities.Product;
+
+
 public class Program{
-    Scanner sc = new Scanner(System.in);
-    
+    public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+        
+		
+		System.out.println("Enter product data:");
+		System.out.print("Name: ");
+		String name = sc.nextLine();
+		System.out.print("Brand: ");
+		String brand = sc.nextLine();
+		System.out.print("Model: ");
+		String model = sc.nextLine();
+		System.out.print("Color: ");
+		String color = sc.nextLine();
+		System.out.print("Obs: ");
+		String obs = sc.nextLine();
+		System.out.print("Price: ");
+		double price = sc.nextDouble();
+		System.out.print("Quantity: ");
+		Product product = new Product(name, brand, model, color, obs, price);
+		int quantity = sc.nextInt();
+        product.addProducts(quantity);
+		
+		System.out.println();
+		System.out.println("Product: " + product.getName() + "\n" + 
+        "Brand: " + product.getBrand() + " | Model: " + product.getModel()  + " | Color: " + product.getColor() + 
+        "\n" + "Obs: " + product.getObs() 
+        + "\n" + "Price: $ " + product.getPrice() + " | Quantity: " + product.getQuantity());
+		
+		sc.close();
+	}
+     		
 }
--- Java/ControleEstoque/entities/Product.java
+++ Java/ControleEstoque/entities/Product.java
@@ -1,5 +1,97 @@
 package entities;
 
 public class Product {
-    
+    private String name;
+    private String brand;
+    private String model;
+    private String color;
+    private String obs;
+    private Double price;
+    private int quantity;
+
+    public Product() {
+    }
+
+    public Product(String name, String brand, String model, String color, String obs, Double price, int quantity) {
+		this.name = name;
+        this.brand = brand;
+        this.model = model;
+        this.color = color;
+		this.obs = obs;
+		this.price = price;
+		this.quantity = quantity;
+	}
+	
+	public Product(String name, String brand, String model, String color, String obs, Double price) {
+		this.name = name;
+        this.brand = brand;
+        this.model = model;
+        this.color = color;
+		this.obs = obs;
+		this.price = price;
+	}
+
+    public void setName (String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+    public void setBrand (String brand) {
+		this.brand = brand;
+	}
+	
+	public String getBrand() {
+		return brand;
+	}
+
+    public void setModel (String model) {
+		this.model = model;
+	}
+	
+	public String getModel() {
+		return model;
+	}
+
+    public void setColor (String color) {
+		this.color = color;
+	}
+	
+	public String getColor() {
+		return color;
+	}
+
+    public void setObs (String obs) {
+		this.obs = obs;
+	}
+	
+	public String getObs() {
+		return obs;
+	}
+	
+	public void setPrice (double price) {
+		this.price = price;
+	}
+	
+	public Double getPrice() {
+		return price;
+	}
+	
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public double totalValueInStock() {
+		return price * quantity;
+	}
+
+	public void addProducts(int quantity) {
+		this.quantity += quantity;
+	}
+
+	public void removeProducts(int quantity) {
+		this.quantity -= quantity;
+	}
 }
