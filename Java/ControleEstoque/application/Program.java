484c711cd652490acdfacaeeec277593c571aa0c
Rafael Pastor <rafa.f.pastor@gmail.com>
2022-04-04 07:45:01 -0300

    Incluida a função para escolher quantos produtos diferentes serão adicionados para adição em uma execução

--- Java/ControleEstoque/application/Program.java
+++ Java/ControleEstoque/application/Program.java
@@ -8,75 +8,103 @@ import entities.Product;
 public class Program{
     public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-
-        
 		
-		System.out.println("Insira as informações do produto");
-		System.out.print("Nome: ");
-		String name = sc.nextLine();
-		System.out.print("Marca: ");
-		String brand = sc.nextLine();
-		System.out.print("Modelo: ");
-		String model = sc.nextLine();
-		System.out.print("Tamanho: ");
-		String size = sc.nextLine();
-		System.out.print("Cor: ");
-		String color = sc.nextLine();
-		System.out.print("Observação: ");
-		String obs = sc.nextLine();
-		System.out.print("Preço: ");
-		double price = sc.nextDouble();
-		System.out.print("Quantidade: ");
-		Product product = new Product(name, brand, model, color, obs, size, price);
-		int quantity = sc.nextInt();
-        product.addProducts(quantity);
+        System.out.print("Insira a quantidade de produtos diferentes a serem inseridos: ");
+		int quant = sc.nextInt();
 		
-		System.out.println();
-		System.out.println(product);
+		for (int x = 1; x <= quant; x++) {
+			System.out.println("Insira as informações do produto #" + x);
+			sc.nextLine();
+			System.out.println();
+			System.out.print("Nome: ");
+			String name = sc.nextLine();
+			System.out.print("Marca: ");
+			String brand = sc.nextLine();
+			System.out.print("Modelo: ");
+			String model = sc.nextLine();
+			System.out.print("Tamanho: ");
+			String size = sc.nextLine();
+			System.out.print("Cor: ");
+			String color = sc.nextLine();
+			System.out.print("Observação: ");
+			String obs = sc.nextLine();
+			System.out.print("Preço: ");
+			double price = sc.nextDouble();
+			System.out.print("Quantidade: ");
+			Product product = new Product(name, brand, model, color, obs, size, price);
+			int quantity = sc.nextInt();
+			product.addProducts(quantity);
+			
+			System.out.println();
+			System.out.println(product);
 
-		sc.nextLine();
-		System.out.println();
-		System.out.print("Deseja adicionar algum item? (S/N) ");
-		String opc = sc.nextLine();
+			sc.nextLine();
+			System.out.println();
+			System.out.print("Deseja adicionar algum item? (S/N) ");
+			String opc = sc.nextLine();
 
-		switch (opc) {
-			case "S":
-				System.out.println();
-				System.out.print("Insira a quantidade que deseja adicionar no estoque: ");
-				quantity = sc.nextInt();
-				
-				product.addProducts(quantity);
-				System.out.println();
-				System.out.println(product);
-			break;
-			case "N":
-				System.out.println("Inclusão Finalizada!");
-			break;
-			default:
-			System.out.println("Opção invalida!");
-			break;
-		}
+			switch (opc) {
+				case "S":
+					System.out.println();
+					System.out.print("Insira a quantidade que deseja adicionar no estoque: ");
+					quantity = sc.nextInt();
+					
+					product.addProducts(quantity);
+					System.out.println();
+					System.out.println(product);
+				break;
+				case "s":
+					System.out.println();
+					System.out.print("Insira a quantidade que deseja adicionar no estoque: ");
+					quantity = sc.nextInt();
+					
+					product.addProducts(quantity);
+					System.out.println();
+					System.out.println(product);
+				break;
+				case "N":
+					System.out.println("Inclusão Finalizada!");
+				break;
+				case "n":
+					System.out.println("Inclusão Finalizada!");
+				break;
+				default:
+				System.out.println("Opção invalida!");
+				break;
+			}
 
-		sc.nextLine();
-		System.out.println();
-		System.out.print("Deseja remover algum item? (S/N) ");
-		String opc2 = sc.nextLine();
+			sc.nextLine();
+			System.out.println();
+			System.out.print("Deseja remover algum item? (S/N) ");
+			String opc2 = sc.nextLine();
 
-		switch (opc2) {
-			case "S":
-				System.out.println();
-				System.out.print("Insira a quantidade que deseja remover no estoque: ");
-				quantity = sc.nextInt();
-				product.removeProducts(quantity);
-				System.out.println();
-				System.out.println(product);
-			break;
-			case "N":
-			System.out.println("Remoção Finalizada!");
-			break;		
-			default:
-			System.out.println("Opção invalida!");
-			break;
+			switch (opc2) {
+				case "S":
+					System.out.println();
+					System.out.print("Insira a quantidade que deseja remover no estoque: ");
+					quantity = sc.nextInt();
+					product.removeProducts(quantity);
+					System.out.println();
+					System.out.println(product);
+				break;
+				case "s":
+					System.out.println();
+					System.out.print("Insira a quantidade que deseja remover no estoque: ");
+					quantity = sc.nextInt();
+					product.removeProducts(quantity);
+					System.out.println();
+					System.out.println(product);
+				break;
+				case "N":
+				System.out.println("Remoção Finalizada!");
+				break;
+				case "n":
+				System.out.println("Remoção Finalizada!");
+				break;		
+				default:
+				System.out.println("Opção invalida!");
+				break;
+			}
 		}
 
 		System.out.println();
